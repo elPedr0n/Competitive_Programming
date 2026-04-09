910d9dda9bdaf6dbea90fdc1d4af0e1f06738b01
Pedron <pepe18616@gmail.com>
2026-04-09 19:22:12 -0300

    finalmente o da moeda ta pago

--- cses_problemset/coin_piles.cpp
+++ cses_problemset/coin_piles.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 using ll = long long;
 
-int main() {
-
+void solve() {
     ll t, a, b, maior, menor;
     cin >> t;
 
@@ -20,13 +19,23 @@ int main() {
             maior = b;
             menor = a;
         }
-
-
-        if (((aa == bb and bb == 0) and ((a == 0 and b == 0) or (a != 0 and b != 0))) or (((aa == 2 and bb == 1) or (aa == 1 and bb == 2)) and (maior / 2 <= menor))) {
-            cout << "YES\n";
-        } else cout << "NO\n";
+        if ((aa == bb and bb != 0) or (aa == 0 and bb != 0) or (aa != 0 and bb == 0) or menor < maior / 2) {
+            cout << "NO\n";
+        } else  cout << "YES\n";
+    
     }
 
+}
+
+int main() {
+
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
 
     return 0;
 }
